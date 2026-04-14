f175a984a055fbed77258f007cb2fe9a4ea4fe95
viviana00425-cell <viviana00425@gmail.com>
2026-04-14 16:08:14 +0100

    改解密

---
+++ Assets/XHQ/Circle Puzzle/Script/GameSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSample : MonoBehaviour
+{
+    public CirclePuzzle circlePuzzle;
+
+    public GameObject showUI;
+
+    void Start ()
+    {
+        circlePuzzle.onComplete += OnComplete;
+    }
+
+    private void OnComplete()
+    {
+        showUI.SetActive(true);
+        circlePuzzle.gamePause = true;
+    }
+
+    public void CloseUI ()
+    {
+        showUI.SetActive(false);
+        circlePuzzle.gamePause = false;
+        circlePuzzle.Setup();
+    }
+}
